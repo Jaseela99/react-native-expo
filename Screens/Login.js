2069f96ef57da8b91b6075821cad12adfb1f9314
Jaseela.A <jaseelachittur@gmail.com>
2022-10-20 09:37:10 +0530

    added login

---
+++ Screens/Login.js
@@ -0,0 +1,64 @@
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from "react-native";
+import React from "react";
+import CheckBox  from "expo-checkbox"
+const Login = () => {
+    const [agree,setAgree] =React.useState(false)
+  return (
+    <View style={styles.mainContainer}>
+      <Text style={styles.mainHeader}>Login Here!!</Text>
+      <View style={styles.inputContainer}>
+        <Text style={styles.labels}>Enter Your Name</Text>
+        <TextInput style={styles.inputStyle} autoCapitalize="none" autoCorrect={false}/>
+      </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.labels}>Enter Your Password</Text>
+        <TextInput style={styles.inputStyle} secureTextEntry={true} autoCapitalize="none" autoCorrect={false}/>
+      </View>
+<View >
+<CheckBox value={agree} on onChange={()=>setAgree(true)} color={agree ? "#4630EB":undefined}/>
+<Text> I agree</Text>
+</View>
+<TouchableOpacity style={{backgroundColor:agree? "#4630EB":"grey"}}>
+    <Text>Login</Text>
+    </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Login;
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    height: "100%",
+    paddingTop: 30,
+    paddingHorizontal: 30,
+    backgroundColor: "#fff",
+  },
+  mainHeader: {
+    fontSize: 25,
+    color: "#344055",
+    fontWeight: 500,
+    paddingTop: 20,
+    paddingBottom: 15,
+  },
+  inputContainer: {
+    marginTop: 20,
+  },
+  labels: {
+    fontSize: 18,
+    color: "#7d7d7d",
+    marginTop: 10,
+    marginBottom: 5,
+    lineHeight: 25,
+    fontFamily: "regular",
+  },
+  inputStyle: {
+    borderWidth: 1,
+    borderColor:"rgba(0,0,0,0.3)",
+    paddingHorizontal:15,
+    paddingVertical:7,
+    borderRadius:1,
+    FontFamily:"regular",
+    fontSize:18
+  },
+});
